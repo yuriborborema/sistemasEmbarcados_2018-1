d4a682f53ba62fb665c873f96493c65140da3257
Jordy Aparecido Faria de Araújo <jordyfaria@hotmail.com>
2018-05-28 16:50:31 -0300

    Mais dois sentidos de rotação
    Coloquei mais dois sentidos de rotação para as leds

--- dance.v
+++ dance.v
@@ -3,17 +3,19 @@ module dance(SW,Clock,led);
 	input Clock;
 	input [17:0] SW;
 	output reg [17:0] led;
-	reg [25:0] slow_count;
+	reg [24:0] slow_count;
 	reg [5:0] position;
+	reg flag; // Determina o sentido de rotação dos leds
 	
-	parameter left=0,right=1,invert=2;
-	
+	parameter left=0,right=1;
+		
 	always @(posedge Clock)
 		slow_count <= slow_count + 1'b1;
 		
 	always @ (posedge Clock) begin
 		if (slow_count == 0) begin
-			if(SW[4]==1) begin	
+			// Direita para esquerda
+			if(SW[0] == 1) begin	
 				if(position < 18) begin
 					if(position != 0) led[position - 1] <= 0;
 					led[position] <= 1;
@@ -22,7 +24,33 @@ module dance(SW,Clock,led);
 					led[position] <= 0;
 					position <= 0;	
 				end
-			end else led[6] <= 1;
+			// Esquerda para direita
+			end else if(SW[1] == 1) begin	
+				if(position > 0) begin
+					if(position != 17) led[position + 1] <= 0;
+					led[position] <= 1;
+					position <= position - 1;
+				end else begin
+					led[position] <=0;
+					position <= 17;	
+				end
+			end
+			// Vai e volta 
+			end else if(SW[2] == 1) begin	
+				if(position > 0 & position < 17) begin					
+					led[position] <= 1;
+					if(way == left) begin
+						if(position != 0) led[position - 1] <= 0;
+						led[position] <= 1;
+						position <= position + 1;
+					end else if(way == right) begin
+						if(position != 17) led[position + 1] <= 0;
+						led[position] <= 1;
+						position <= position - 1;
+					end
+				end else if(position == 0) way = left;
+				else if (position == 17) way == right;			
+			end			
 		end
 	end	
 endmodule 
