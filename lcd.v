6f4f90f3fe00f4cb0af73d3462718f1a84d86921
yuriborborema <yuriborborema10@gmail.com>
2018-06-12 08:57:42 -0600

    testando com initial

--- lcd.v
+++ lcd.v
@@ -2,18 +2,30 @@ module lcd(clk, LCD_data_in, LCD_DATA, LCD_RS, LCD_RW, LCD_EN);
 
 	input clk; 
 	input [7:0]LCD_data_in;
-	output LCD_RS, LCD_RW;
-	output reg LCD_EN;
-	output [7:0] LCD_DATA;
+	output LCD_RS;
+	output reg LCD_EN, LCD_RW;
+	output reg[7:0] LCD_DATA;
 
 
-	assign LCD_RW = 0;
+	//assign LCD_RW = 0;
 	assign LCD_RS = 1;
-	assign LCD_DATA = LCD_data_in;
-
+	initial begin
+	LCD_RW=0;
+	LCD_DATA=8'h38;
+	#10;
+	LCD_DATA=8'h0F;
+	
+	end
+	
+	//assign LCD_DATA = LCD_data_in;
+	
+	reg [2:0] count;
 	always @(posedge clk) begin
-		LCD_EN <= 1;
-		//LCD_E <= 0;
+	if(count!=0) count <= count + 1;
+	end
+	
+	always @(posedge clk) begin
+		LCD_EN <= (count!=0);
 	end
 
 endmodule 
